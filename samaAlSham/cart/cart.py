df5d29837bbdd01828e51b7e8ec5815fae013d63
sama <samashalabi2018@gmail.com>
2024-11-25 00:37:30 +0200

    17 + error

--- samaAlSham/cart/cart.py
+++ samaAlSham/cart/cart.py
@@ -15,13 +15,14 @@ class Cart:
         # make sure cart available in all [ages of the website
         self.cart = cart
 
-    def add(self, product):
+    def add(self, product, quantity):
         product_id = str(product.id)
+        product_qty = str(quantity)
         if product_id in self.cart:
             pass
 
         else:
-            self.cart[product_id] = {"price": str(product.price)}
+            self.cart[product_id] = int(product_qty)
 
         self.session.modified = True
 
@@ -33,3 +34,21 @@ class Cart:
         products = Product.objects.filter(id__in=product_ids)
 
         return products
+
+    def get_quants(self):
+        quantities = self.cart
+
+        return quantities
+
+    def update(self, product, quantity):
+        product_id = str(product)
+        product_qty = int(quantity)
+
+        ourCart = self.cart
+
+        # update cart => product_id value into new product_qty
+        ourCart[product_id] = product_qty
+
+        self.session.modified = True
+
+        return self.cart
--- samaAlSham/cart/views.py
+++ samaAlSham/cart/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404,redirect
 from .cart import Cart
 from store.models import Product
 from django.http import JsonResponse
@@ -6,35 +6,50 @@ from django.http import JsonResponse
 
 # Create your views here.
 def cart_summary(request):
-    cart=Cart(request)
-    cart_products=cart.get_prods 
-    return render(request, "cart_summary.html", {'cart_products':cart_products})
+    cart = Cart(request)
+    cart_products = cart.get_prods
+    quantities = cart.get_quants
+    return render(
+        request,
+        "cart_summary.html",
+        {"cart_products": cart_products, "quantities": quantities},
+    )
 
 
 def cart_add(request):
     cart = Cart(request)
     if request.POST.get("action") == "post":
-        
+
         product_id = int(request.POST.get("product_id"))
+        product_qty = int(request.POST.get("product_qty"))
         # lookup for product in the DB
         product = get_object_or_404(Product, id=product_id)
 
         # save to session
-        cart.add(product=product)
+        cart.add(product=product, quantity=product_qty)
 
-        cart_quantity=cart.__len__()
+        cart_quantity = cart.__len__()
 
         response = JsonResponse({"qty": cart_quantity})
-        
+
         return response
-    
+
     else:
         return JsonResponse({"error": "Invalid request"})
-    
 
 
 def cart_update(request):
-    pass
+    cart = Cart(request)
+    if request.POST.get("action") == "post":
+
+        product_id = int(request.POST.get("product_id"))
+        product_qty = int(request.POST.get("product_qty"))
+
+        cart.update(product=product_id, quantity=product_qty)
+
+        response = JsonResponse({"qty": product_qty})
+        return response
+        # return redirect ('cart_summary')
 
 
 def cart_delete(request):
