b4955eb9c2696ddfdcd7e89baf0bf079e0d7ecaa
sama <samashalabi2018@gmail.com>
2024-12-06 15:28:06 +0200

    half 34

--- samaAlSham/payment/models.py
+++ samaAlSham/payment/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from store.models import Product
+from django.db.models.signals import post_save
 
 
 # Create your models here.
@@ -8,7 +10,7 @@ class ShippingAddress(models.Model):
     shipping_full_name = models.CharField(max_length=255)
     shipping_email = models.CharField(max_length=255)
     shipping_address1 = models.CharField(max_length=255)
-    shipping_address2 = models.CharField(max_length=255,null=True,blank=True)
+    shipping_address2 = models.CharField(max_length=255, null=True, blank=True)
     shipping_city = models.CharField(max_length=255)
     shipping_state = models.CharField(max_length=255, null=True, blank=True)
     shipping_zipcode = models.CharField(max_length=255, null=True, blank=True)
@@ -19,3 +21,35 @@ class ShippingAddress(models.Model):
 
     def __str__(self):
         return f"Shipping Address - {str(self.id)}"
+
+
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    order_full_name = models.CharField(max_length=255)
+    order_email = models.CharField(max_length=255)
+    shipping_address = models.TextField(max_length=15000)
+    amount_paid = models.DecimalField(max_digits=10, decimal_places=2)
+    date_ordered = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Order - {str(self.id)} "
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, null=True)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    quantity = models.PositiveBigIntegerField(default=1)
+    price = models.DecimalField(max_digits=7, decimal_places=2)
+
+    def __str__(self):
+        return f"Order Item - {str(self.id)}"
+
+
+def create_shipping(sender, instance, created, **kwargs):
+    if created:
+        user_shipping = ShippingAddress(user=instance)
+        user_shipping.save()
+
+
+post_save.connect(create_shipping, sender=User)
--- samaAlSham/payment/views.py
+++ samaAlSham/payment/views.py
@@ -1,6 +1,68 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from cart.cart import Cart
+from payment.froms import ShippingForm
+from payment.models import ShippingAddress
+from django.contrib import messages
 
 
 # Create your views here.
 def payment_success(request):
     return render(request, "payment/payment_success.html", {})
+
+
+def checkout(request):
+    cart = Cart(request)
+    cart_products = cart.get_prods
+    quantities = cart.get_quants
+    totals = cart.cart_total()
+
+    if request.user.is_authenticated:
+        shipping_user = ShippingAddress.objects.get(user__id=request.user.id)
+        shipping_form = ShippingForm(request.POST or None, instance=shipping_user)
+        return render(
+            request,
+            "payment/checkout.html",
+            {
+                "cart_products": cart_products,
+                "quantities": quantities,
+                "totals": totals,
+                "shipping_form": shipping_form,
+            },
+        )
+    else:
+        shipping_user = ShippingAddress.objects.get(user__id=request.user.id)
+        shipping_form = ShippingForm(request.POST or None, instance=shipping_user)
+        return render(
+            request,
+            "payment/checkout.html",
+            {
+                "cart_products": cart_products,
+                "quantities": quantities,
+                "totals": totals,
+                "shipping_form": shipping_form,
+            },
+        )
+
+
+def billing_info(request):
+    if request.POST:
+        cart = Cart(request)
+        cart_products = cart.get_prods
+        quantities = cart.get_quants
+        totals = cart.cart_total()
+
+        shipping_user = ShippingAddress.objects.get(user__id=request.user.id)
+        shipping_form = ShippingForm(request.POST or None, instance=shipping_user)
+        return render(
+            request,
+            "payment/billing_info.html",
+            {
+                "cart_products": cart_products,
+                "quantities": quantities,
+                "totals": totals,
+                "shipping_form": shipping_form,
+            },
+        )
+    else:
+        messages.success(request, ("يجب ان تسجل الدخول اولا"))
+        return redirect("home")
